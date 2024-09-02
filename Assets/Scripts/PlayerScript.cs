8d79c84d64b36d726e4c8879e6739ddbe16f3667
JeronimoLedesma <ledesmajero@gmail.com>
2024-09-02 18:28:14 -0300

    Clase 2
    Añadido el salto limitado y plano de muerte/reinicio de escena de la clase 2

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,16 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
     public float speed;
+    public float jumpForce;
     public Vector2 inputVector;
     public Rigidbody rigidBody;
+    bool canJump;
     // Start is called before the first frame update
     void Start()
     {
        rigidBody = GetComponent<Rigidbody>();
+       canJump = true;
     }
 
     // Update is called once per frame
@@ -19,5 +23,27 @@ public class PlayerScript : MonoBehaviour
         inputVector.x = Input.GetAxis("Horizontal");
         inputVector.y = Input.GetAxis("Vertical");
         rigidBody.AddForce(inputVector.x * speed, 0f, inputVector.y * speed, ForceMode.Impulse);
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump) { 
+
+            rigidBody.AddForce(0f, jumpForce, 0f, ForceMode.Impulse);
+            canJump = false;
+        }
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Floor"))
+        {
+            canJump = true;
+        }
+
+        if (collision.gameObject.CompareTag("deathPlane"))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
+
+
